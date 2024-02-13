e4cebb33941bbecdc14fab85c498e8266cd6fde8
Guillaume Boyer <guiboy97.410@gmail.com>
2024-02-13 13:38:03 +0100

    exo

--- my-file.js
+++ my-file.js
@@ -1,1 +1,17 @@
-var age = 10;
+// function addition(x: number, y: number): number {
+//     return x + y;
+// }
+// const result:number = addition(10, 20);
+// console.log(result);
+function damage(characterToDamage, _a, amount) {
+    var number = _a.life;
+    characterToDamage.life -= amount;
+    return characterToDamage.life;
+}
+var result = damage({ life: 100 }, 12);
+console.log(result);
+// Ne modifiez que le typage TypeScript, ne touchez pas au code JavaScript !
+function sayHello(target) {
+    return "Hello ".concat(target.firstname, ", you have ").concat(target.money, " on your bank account.");
+}
+var message = sayHello({ firstname: 'World', money: 123 });
--- my-file.ts
+++ my-file.ts
@@ -1,1 +1,19 @@
-const age: number = 10;
+// function addition(x: number, y: number): number {
+//     return x + y;
+// }
+// const result:number = addition(10, 20);
+// console.log(result);
+
+function damage(characterToDamage { life:number }, amount:number):number {
+    characterToDamage.life -= amount;
+    return characterToDamage.life;
+}
+const result = damage({ life: 100 }, 12);
+console.log(result);    
+
+// Ne modifiez que le typage TypeScript, ne touchez pas au code JavaScript !
+
+function sayHello(target: { firstName: string, money: number }):string {
+    return `Hello ${target.firstname}, you have ${target.money} on your bank account.`;
+}
+const message: string = sayHello({ firstname: 'World', money: 123 });
